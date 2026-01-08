365950dd2cf3d36bf39bf1d1bf7110f4ca25ea52
Pankajnegi06 <pankajsnegi06@gmail.com>
2026-01-08 11:33:22 +0530

    removed uploads

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/pipeline';
+const API_URL = 'https://smart-b-roll-inserter-1.onrender.com/api/pipeline';
 
 export const api = {
   // Status
